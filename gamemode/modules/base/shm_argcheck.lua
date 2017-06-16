b4b2b11d229a63556e829d8a5fef0e3209a60065
Samuel Moriarty <insomnia.array@gmail.com>
2017-06-17 01:30:30 +0400

    Reimplement argcheck macros

--- gamemode/modules/base/shm_argcheck.lua
+++ gamemode/modules/base/shm_argcheck.lua
@@ -1,33 +1,36 @@
 #module base.argcheck
 
 alpha.preprocess.add_directive("check",
-	function(a, b, c)
-		print(a, b, c)
+	function(text)
+		if (DEBUG) then
+			alpha.preprocess.inject(([[validate_type(%s)]]):format(text))
+		end
 	end)
 
--- function argcheck:initialize()
--- 	function validate_type(arg, ...)
--- 		#if DEBUG then
--- 			local acceptable_types = {...}
---
--- 			local match = false
--- 			for _, arg_type in pairs(acceptable_types) do
--- 				if (type(arg) == arg_type) then
--- 					match = true
--- 					break
--- 				end
--- 			end
---
--- 			if (!match) then
--- 				error(string.format("Invalid argument, (%s) expected but got %s", table.concat(acceptable_types, ", "), type(arg)))
--- 			end
--- 		#end
--- 	end
---
--- 	function validate_subtype(arg, arg_type)
--- 		#if DEBUG then
--- 			assert(arg.is_instance_of != nil, "Invalid argument, expect an object but got something else")
--- 			assert(arg:is_instance_of(arg_type), "Invalid argument, object must be derived from " .. arg_type)
--- 		#end
--- 	end
--- end
+alpha.preprocess.add_directive("checksub",
+	function(text)
+		if (DEBUG) then
+			alpha.preprocess.inject(([[validate_subtype(%s)]]):format(text))
+		end
+	end)
+
+function validate_type(arg, ...)
+	local acceptable_types = {...}
+
+	local match = false
+	for _, arg_type in pairs(acceptable_types) do
+		if (type(arg) == arg_type) then
+			match = true
+			break
+		end
+	end
+
+	if (!match) then
+		error(string.format("Invalid argument, (%s) expected but got %s", table.concat(acceptable_types, ", "), type(arg)))
+	end
+end
+
+function validate_subtype(arg, arg_type)
+	assert(arg.is_instance_of != nil, "Invalid argument, expected an object but got something else")
+	assert(arg:is_instance_of(arg_type), "Invalid argument, object must be derived from " .. arg_type)
+end
